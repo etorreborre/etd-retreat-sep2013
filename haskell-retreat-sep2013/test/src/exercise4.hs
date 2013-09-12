9a42de9c218fb71a42078dd6e3293e6515680353
etorreborre <etorreborre@yahoo.com>
2013-09-12 11:10:38 +1000

    finished exercise 4

---
+++ haskell-retreat-sep2013/test/src/exercise4.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import System.Environment
+import Data.List
+import Control.Applicative
+import Data.Maybe
+
+main :: IO ()
+main = do
+   n : _ <- getArgs
+   putStrLn $ show $ difference $ read n
+
+difference :: Int -> Int
+difference n = (squareSum n) - (sumSquares n)
+
+sumSquares :: Int -> Int
+sumSquares n = sum $ map (^2) [1 .. n]
+
+squareSum :: Int -> Int
+squareSum n = (sum [1 .. n]) ^ 2
+
+
