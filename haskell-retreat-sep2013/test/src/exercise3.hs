6a0e0ce539a5e3f9b44e7b0f7ff3419550841891
etorreborre <etorreborre@yahoo.com>
2013-09-12 11:05:05 +1000

    finished exercise 3

---
+++ haskell-retreat-sep2013/test/src/exercise3.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import System.Environment
+import Data.List
+import Control.Applicative
+import Data.Maybe
+
+main :: IO ()
+main = do
+   s : _ <- getArgs
+   putStrLn $ encode s
+
+encode :: String -> String
+encode string =  string >>= encodeChar
+
+encodeChar :: Char -> String
+encodeChar c = fromMaybe [c] (findCorresponding c)
+
+findCorresponding :: Char -> Maybe String
+findCorresponding c = lookup c [('+', "%2B"), (' ', "%20"), ('#', "%23"), ('%', "%25")]
+
+
+
